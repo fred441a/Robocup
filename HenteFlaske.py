e9982eac362ebcbd6c176ffab6fbb3b14c9be5b5
fred441a <frederik@altofte.dk>
2020-09-15 13:22:11 +0200

    Fixed bugs?

--- HenteFlaske.py
+++ HenteFlaske.py
@@ -19,14 +19,7 @@ def løfte_flaske(Arm_Motor, openclose):
         print(Arm_Motor.angle())
         Arm_Motor.run_target(200,1300, then=Stop.HOLD, wait=True)
     else:
-<<<<<<< HEAD
-        Arm_Motor.run_target(200,-1300, then=Stop.HOLD, wait=True)
-
-def sænk_flaske(Arm_Motor, openclose):
-    
-=======
         Arm_Motor.run_target(200,0, then=Stop.HOLD, wait=True)
->>>>>>> 23c181d8d97b0af7ed2556e6bb90f7adec0857e4
    
 
 
