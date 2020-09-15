76b5c457f208f0019c0e8a049dacc584b3a82006
Rikke Udengaard <rikkeudengaard@h449.aau1x.klient2.satellit.site.aau.dk>
2020-09-15 14:20:43 +0200

    g

--- RundtomFlaske.py
+++ RundtomFlaske.py
@@ -1,23 +1,21 @@
 from Ligeud import Kør_Lige_ud
-from pybricks.ev3devices import Motor, ColorSensor,  UltrasonicSensor
-from pybricks.robotics import DriveBase
 
 def Rundt_Om_Flaske(DriveBase,sensor,grey):
-    DriveBase.turn(45)
-    DriveBase.straight(250)
-    DriveBase.turn(-45)
-    DriveBase.straight(250)
+    robot.turn(45)
+    robot.straight(250)
+    robot.turn(-45)
+    robot.straight(250)
     print(grey)
     while line_sensor.reflection() > grey:
         print(line_sensor.reflection())
-        DriveBase.drive(100,0)
-    DriveBase.stop()
-    DriveBase.turn(45)
+        robot.drive(100,0)
+    robot.stop()
+    robot.turn(45)
 
 def SjetteSegment(robot,line_sensor,threshold,BLACK):
-    Rundt_Om_Flaske(DriveBase,sensor,BLACK)
+    Rundt_Om_Flaske(robot,sensor,BLACK)
     Kør_Lige_ud(robot,line_sensor,threshold,-2)
 
 def OttendeSegment(robot,line_sensor,threshold,BLACK):
-    Rundt_Om_Flaske(DriveBase,sensor,BLACK)
+    Rundt_Om_Flaske(robot,sensor,BLACK)
     Kør_Lige_ud(robot,line_sensor,threshold,2)
