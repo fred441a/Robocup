2b073ac6fe4f3759dc0cc225bde355d6b969078a
jakobjespersen <jakobjespersen@mail.com>
2020-09-22 14:36:50 +0200

    Jeg_slettede_main_funktionerne_fix

--- main.py
+++ main.py
@@ -53,7 +53,30 @@ while True:
         ev3.speaker.beep()
     if BLACK != None and WHITE != None:
         threshold = (BLACK + WHITE) / 2
+        FørsteSegment(robot,line_sensor,threshold,BLACK)
+        ev3.speaker.beep()
+        print("Færdig med første segment")
+        Kør_Lige_ud(robot,line_sensor,threshold,2)
+        beeper(ev3,2)
+        Kør_Lige_ud(robot,line_sensor,threshold,2)
+        beeper(ev3,3)
         FireGrå(robot,line_sensor,BLACK,WHITE)
+        beeper(ev3,4)
+        print("Færdig med fire grå")
+        Kør_Lige_ud(robot,line_sensor,threshold,2)
+        beeper(ev3,5)
+        SjetteSegment(robot,line_sensor,threshold,BLACK)
+        beeper(ev3,6)
+        print("Færdig med sjette segment")
+        SyvendeSegment(robot,ultra_sensor,ev3)
+        beeper(ev3,7)
+        Kør_Lige_ud(robot,line_sensor,threshold,2)
+        beeper(ev3,8)
+        print("Færdig med syvende segment")
+        OttendeSegment(robot,line_sensor,threshold,BLACK)
+        beeper(ev3,9)
+        print("ottende segment")
+        break
         
 def beeper(ev3,times):
     integer = times
