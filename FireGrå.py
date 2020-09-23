1f7d1b3292e80e4db45fb8547f461e2be802a010
jakobjespersen <jakobjespersen@mail.com>
2020-09-23 15:00:18 +0200

    firegrå fix

--- FireGrå.py
+++ FireGrå.py
@@ -1,14 +1,14 @@
 from Ligeud import Kør_Lige_ud
 
 def SerIkkeGrå1(robot, sensor, grey, white, ev3):
-    print("den ser grå1")
+    print("SER GRÅ, LEDER EFTER HVID")
     threshold = (grey+white)/2
     ev3.speaker.beep()
     robot.turn(-10)
 
     if(sensor.reflection() > grey):
         robot.straight(190)
-        print("Den ser hvid og kører ligeud 17.5cm")
+        print("HAR FUNDET HVID")
 
         if(sensor.reflection() > grey):
             robot.straight(175)
@@ -35,16 +35,18 @@ def FireGrå(robot, sensor, grey, white,ev3):
     threshold = (grey+white)/2
     robot.straight(500)
     robot.turn(-45)
-    print("Den turner første gang")
+    print("TURNER 1. GANG")
 
     if(sensor.reflection() > grey):
+        print("SER HVID, ALT ER GODT")
         robot.straight(190)
-        print("Den ser hvid og kører ligeud 17.5cm")
+        
 
         if(sensor.reflection() > grey):
+            print("SER HVID IGEN, ALT ER GODT")
             robot.straight(175)
             robot.turn(45)
-            print("Den turner anden gang")
+            
             Kør_Lige_ud(robot,sensor,threshold,-3)
 
         else:
