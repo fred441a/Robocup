3ca14fc24fb08bf5428ded8538a91dbb9917a03d
jakobjespersen <jakobjespersen@mail.com>
2020-09-25 13:34:51 +0200

    fix på stop på midten

--- Stoppåmidten.py
+++ Stoppåmidten.py
@@ -1,7 +1,7 @@
 from Ligeud import Kør_Lige_ud
 
 def stop_på_midten(DriveBase, ultra_sensor):
-    Kør_Lige_ud(robot,line_sensor,threshold,-2)
+    Kør_Lige_ud(robot,line_sensor,threshold,-3)
     if ultra_sensor.distance() < 150:
             DriveBase.stop()
             print("afstand fundet")
@@ -10,8 +10,6 @@ def stop_på_midten(DriveBase, ultra_sensor):
     robot.straight(-1275)
     robot.turn(-90)
     robot.straight(30)
-
-
 
 
     
