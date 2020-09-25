ee417dede1c1c7fc8cbfe473501c38d6bbd4f607
jakobjespersen <jakobjespersen@mail.com>
2020-09-25 10:01:13 +0200

    Optimering af firegrå

--- FireGrå.py
+++ FireGrå.py
@@ -11,23 +11,10 @@ def SerIkkeGrå1(robot, sensor, grey, white, ev3):
         print("HAR FUNDET HVID")
 
         if(sensor.reflection() > grey):
-            robot.straight(175)
+            robot.straight(365)
             robot.turn(45)
             print("Den turner anden gang")
             Kør_Lige_ud(robot,sensor,threshold,-4)
-
-def SerIkkeGrå2(robot, sensor, grey, white, ev3):
-    print("den ser grå2")
-    threshold = (grey+white)/2
-    ev3.speaker.beep()
-    robot.turn(-10)
-
-    if(sensor.reflection() > grey):
-        robot.straight(175)
-        robot.turn(45)
-        print("Den turner anden gang")
-        Kør_Lige_ud(robot,sensor,threshold,-4)
-
 
 
 
@@ -39,19 +26,12 @@ def FireGrå(robot, sensor, grey, white,ev3):
 
     if(sensor.reflection() > grey):
         print("SER HVID, ALT ER GODT")
-        robot.straight(190)
+        robot.straight(365)
+        print("TURNER 2. GANG")
+        robot.turn(30)
+        Kør_Lige_ud(robot,sensor,threshold,-4)
         
-
-        if(sensor.reflection() > grey):
-            print("SER HVID IGEN, ALT ER GODT")
-            robot.straight(175)
-            robot.turn(30)
-            
-            Kør_Lige_ud(robot,sensor,threshold,-4)
-
-        else:
-            SerIkkeGrå2(robot, sensor, grey, white, ev3)
-            
+     
     else:
             SerIkkeGrå1(robot, sensor, grey,white, ev3)
             
