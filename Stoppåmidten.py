66f17d84ba8e6bd7da1810e6fb3ed24d24acee41
jakobjespersen <jakobjespersen@mail.com>
2020-09-28 10:54:10 +0200

    stop på midten

--- Stoppåmidten.py
+++ Stoppåmidten.py
@@ -1,11 +1,13 @@
 from Ligeud import Kør_Lige_ud_midten
 
 def stop_på_midten(robot, line_sensor, ultra_sensor, threshold):
-    Kør_Lige_ud_midten(robot, line_sensor,threshold,-3, ultra_sensor)
-    robot.reset()
-    robot.straight(-1275)
-    robot.turn(-90)
-    robot.straight(30)
+        robot.turn(30)
+        robot.straight(25)
+        Kør_Lige_ud_midten(robot, line_sensor,threshold,-3, ultra_sensor)
+        robot.reset()
+        robot.straight(-1700)
+        robot.turn(-90)
+        robot.straight(30)
 
 
     
