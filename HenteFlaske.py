7ff75e8a639f02fae5724c289c09b267e9e80aa1
Rikke Udengaard <rikkeudengaard@h490.aau1x.klient2.satellit.site.aau.dk>
2020-09-28 18:21:14 +0200

    g

--- HenteFlaske.py
+++ HenteFlaske.py
@@ -3,32 +3,32 @@ from Ligeud import Kør_Lige_ud
 from pybricks.tools import wait
 
 def dreje_mod_flaske(drivebase):
-    DriveBase.straight(250)
-    DriveBase.turn(90)
+    drivebase.straight(250)
+    drivebase.turn(90)
 
 #den kører frem og får flasken ind i grebet. Vi har målt at der er 8 cm fra sensor til flasken
-def Kør_hen_til_flaske(DriveBase,ultra_sensor):
+def Kør_hen_til_flaske(drivebase,ultra_sensor):
     while True:
         if ultra_sensor.distance() < 115:
-            DriveBase.stop()
+            drivebase.stop()
             print("afstand fundet")
             break
         else:
-            DriveBase.drive(100,0)
+            drivebase.drive(100,0)
 
 #set openclose til tru hvis den skal åbne og false hvis den skal lukke
 def løfte_flaske(Arm_Motor, openclose):
     if openclose:
         print(Arm_Motor.angle())
-        Arm_Motor.run_target(300,1500, then=Stop.HOLD, wait=True)
+        Arm_Motor.run_target(300,1500)
     else:
-        Arm_Motor.run_target(300,0, then=Stop.HOLD, wait=True)
+        Arm_Motor.run_target(300,0)
 
 def Kør_indtil_sort(drivebase,line_sensor):
     while True:
         drivebase.drive(200,0)
         if line_sensor.reflection() < 15:
-            drivebase.straight(40)
+            drivebase.straight(60)
             drivebase.stop()
             break
         wait(5)
