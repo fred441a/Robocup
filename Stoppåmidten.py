6e979df654979d02100586d0b98d4641d55a95ba
jakobjespersen <jakobjespersen@mail.com>
2020-09-29 10:30:56 +0200

    test push

--- Stoppåmidten.py
+++ Stoppåmidten.py
@@ -1,8 +1,8 @@
 from Ligeud import Kør_Lige_ud_midten
 
 def stop_på_midten(robot, line_sensor, ultra_sensor, threshold):
-        robot.turn(30)
-        robot.straight(250)
+        robot.turn(35)
+        robot.straight(350)
         Kør_Lige_ud_midten(robot, line_sensor,threshold,-3, ultra_sensor)
         robot.reset()
         robot.straight(-1800)
