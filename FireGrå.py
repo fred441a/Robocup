e12c22afc098863dd19ef6e803d69ef056ddae85
jakobjespersen <jakobjespersen@mail.com>
2020-09-29 08:32:56 +0200

    fire grå push

--- FireGrå.py
+++ FireGrå.py
@@ -1,32 +1,37 @@
 from Ligeud import Kør_Lige_ud
 
-#def SerIkkeGrå1(robot, sensor, grey, white, ev3):
-    #print("SER GRÅ, LEDER EFTER HVID")
-    #threshold = (grey+white)/2
-    #ev3.speaker.beep()
-    #robot.turn(-10)
+def SerIkkeGrå1(robot, sensor, grey, white, ev3):
+    print("SER GRÅ, LEDER EFTER HVID")
+    threshold = (grey+white)/2
+    ev3.speaker.beep()
+    robot.turn(-10)
 
-    #if(sensor.reflection() > grey):
-        #robot.straight(150)
-        #print("HAR FUNDET HVID")
+    if(sensor.reflection() > grey):
+        robot.straight(190)
+        print("HAR FUNDET HVID")
 
-        #if(sensor.reflection() > grey):
-            #robot.straight(365)
-            #robot.turn(45)
-            #print("Den turner anden gang")
-            #Kør_Lige_ud(robot,sensor,threshold,-2)
+        if(sensor.reflection() > grey):
+            robot.straight(365)
+            robot.turn(45)
+            print("Den turner anden gang")
+            Kør_Lige_ud(robot,sensor,threshold,-4)
 
 
 
-def FireGrå(drivebase,ultra_sensor,line_sensor,grey, white):
+def FireGrå(robot, sensor, grey, white,ev3):
+    threshold = (grey+white)/2
     robot.straight(500)
     robot.turn(-45)
-    robot.straight(400)
     print("TURNER 1. GANG")
+    robot.straight(340)
+
+    while (sensor.reflection() > grey):
+        robot.drive(150,0)
+        print("SER HVID, ALT ER GODT")
+
     robot.turn(30)
-    Kør_Lige_ud(robot,sensor,threshold,-2)
-        
-            
+    print("TURNER 2. GANG")
+    Kør_Lige_ud(robot,sensor,threshold,-4)
 
 
 
